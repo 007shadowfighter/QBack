10e250386b13954f648c6657191b590e2d6f3286
007shadowfighter <007shadowfighter@gmail.com>
2018-12-10 15:35:30 -0800

    cheer sound ingame and helmet texure changed

---
+++ Assets/Scripts/Catch.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+public class CatchAnimation : MonoBehaviour
+{
+
+    Animator anim;
+    
+
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+        //anim.enabled = false;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == ("Player"))
+        {
+            //anim.enabled = true;
+            anim.Play("Run");
+        }
+    }
+}
---
+++ Assets/Scripts/CatchAnimation.cs
@@ -0,0 +1,21 @@
+﻿//using UnityEngine;
+//public class CatchAnimation : MonoBehaviour
+//{
+
+//    Animator anim;
+//    AudioClip sound;
+
+//    void Start()
+//    {
+//        anim.enabled = false;
+//    }
+
+//    void OnTriggerEnter(Collider col)
+//    {
+//        if (col.gameObject.tag == ("Player"))
+//        {
+//            anim.enabled = true;
+//            AudioSource.PlayClipAtPoint(sound, transform.position);
+//        }
+//    }
+//}
